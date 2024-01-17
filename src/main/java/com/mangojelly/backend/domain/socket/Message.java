ab1346ff4a4ad77a6716766a84b3260cbef069ff
slcloe <67732600+slcloe@users.noreply.github.com>
2024-01-17 11:18:22 +0900

    Feat: #7 "1:N 통신(STOMP)구현"

--- src/main/java/com/mangojelly/backend/domain/socket/Message.java
+++ src/main/java/com/mangojelly/backend/domain/socket/Message.java
@@ -12,16 +12,6 @@ import lombok.NoArgsConstructor;
 public class Message {
     private String type;
     private String sender;
-    private String receiver;
+    private String channelId;
     private Object data;
-
-    public void setSender(String sender) {this.sender = sender;}
-
-    public void newConnect() {
-        this.type = "new";
-    }
-
-    public void closeConnect() {
-        this.type = "close";
-    }
 }
---
+++ src/main/java/com/mangojelly/backend/domain/socket/SpringWebSocketStompApplication.java
@@ -0,0 +1,11 @@
+package com.mangojelly.backend.domain.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketStompApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SpringWebSocketStompApplication.class, args);
+    }
+}
