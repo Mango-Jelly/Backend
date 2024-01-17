2c9e66410b195cb902b83cff66179a2c4393d719
slcloe <67732600+slcloe@users.noreply.github.com>
2024-01-17 12:47:26 +0900

    Feat: #7 channel Id : uri pathVarible로 수정

--- src/main/java/com/mangojelly/backend/domain/socket/MessageController.java
+++ src/main/java/com/mangojelly/backend/domain/socket/MessageController.java
@@ -1,19 +1,24 @@
 package com.mangojelly.backend.domain.socket;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MessageController {
 
     private final SimpMessageSendingOperations simpMessageSendingOperations;
 
-    @MessageMapping("/hello")
-    public void message(Message message){
-        simpMessageSendingOperations.convertAndSend("/sub/channel/" + message.getChannelId(), message);
+    @MessageMapping("/hello/{channelId}")
+    public void message(Message message, @DestinationVariable("channelId") String channelId){
+        log.debug("channelId:{}", channelId);
+        simpMessageSendingOperations.convertAndSend("/sub/channel/" + channelId, message);
     }
 
 }
