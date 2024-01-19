284dfb4972caaf940a090264345b17d2711c303c
slcloe <67732600+slcloe@users.noreply.github.com>
2024-01-19 11:04:39 +0900

    Refactor: #5 RoomCreateReqiest dpt -> department 수정

--- src/main/java/com/mangojelly/backend/applicatoin/dto/request/RoomCreateRequest.java
+++ src/main/java/com/mangojelly/backend/applicatoin/dto/request/RoomCreateRequest.java
@@ -2,9 +2,9 @@ package com.mangojelly.backend.applicatoin.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record RoomCreateRequest(@NotBlank String title, @NotBlank String dpt){
-    public RoomCreateRequest(String title, String dpt){
+public record RoomCreateRequest(@NotBlank String title, @NotBlank String department){
+    public RoomCreateRequest(String title, String department){
         this.title = title;
-        this.dpt = dpt;
+        this.department = department;
     }
 }
