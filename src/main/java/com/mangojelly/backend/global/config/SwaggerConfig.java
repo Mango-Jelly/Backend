8077c2676615f249b04479ccc02a2072048acf31
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2024-02-07 13:38:56 +0900

    Feat(#47):Swagger Server추가

--- src/main/java/com/mangojelly/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/mangojelly/backend/global/config/SwaggerConfig.java
@@ -2,17 +2,27 @@ package com.mangojelly.backend.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @OpenAPIDefinition(
+        servers = {
+                @Server(
+                        url = "https://mangotail.shop", description = "https가 적용된 URL"
+                ),
+                @Server(
+                        url = "http://mangotail.shop", description = "http가 적용된 URL"
+                )
+        },
         info =
         @Info(
                 title = "Mango-Jelly API Document",
                 description = "Mango-Jelly 프로젝트의 API 명세서",
                 version = "v1"))
+
 @Configuration
 public class SwaggerConfig {
     @Bean
