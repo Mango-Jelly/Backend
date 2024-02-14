0e2e1f6137477051bbd7cfe5c13d766456ff041f
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2024-02-14 14:25:05 +0900

    Fix(#29):movie Editor

--- src/main/resources/util/VideoConcater.py
+++ src/main/resources/util/VideoConcater.py
@@ -1,16 +1,15 @@
 import sys
 import os
 import argparse
-import ffmpeg
-from moviepy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips
 
-def concatenate_videos(output_path,video_paths):
+def concatenate_videos(output_path,*video_paths):
     if not video_paths:
         print("적어도 하나 이상의 비디오 파일 경로를 제공해야 합니다.")
         return
 
     try:
-        video_clips = [(VideoFileClip(video_path).resize(height=720)).set_fps(30) for video_path in video_paths]
+        video_clips = [(VideoFileClip(video_path)).set_fps(28) for video_path in video_paths]
         final_clip = concatenate_videoclips(video_clips)
         final_clip.write_videofile(output_path, codec="libx264", ffmpeg_params=['-c:v', 'libx264', '-crf', '18'])
 
@@ -18,25 +17,9 @@ def concatenate_videos(output_path,video_paths):
         print(f"오류 발생: {e}")
         delete_videos(video_paths)
 
-def convert_to_mp4(*video_paths):
-    converted_files = []
-    for video_path in video_paths:
-        filename, file_extension = os.path.splitext(video_path)
-        output_file = f"{filename}.mp4"
-        try:
-            ffmpeg.input(video_path).output(output_file).run()
-            print(f"File converted successfully: {output_file}")
-            converted_files.append(output_file)
-        except ffmpeg.Error as e:
-            print(f"Error occurred: {e.stderr}")
-            return
-    return converted_files
-
-
 def delete_videos(*video_paths):
     for video_path in video_paths:
         os.remove(video_path)
-        # print(f"{video_path}를 삭제했습니다.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Concatenate videos and optionally delete input videos.")
@@ -45,12 +28,11 @@ if __name__ == "__main__":
     parser.add_argument("video_paths", nargs="+", help="Paths of input videos to be concatenated")
     args = parser.parse_args()
 
-    converted_files = convert_to_mp4(*args.video_paths)
-    concatenate_videos(args.output_video, converted_files)
+    concatenate_videos(args.output_video, *args.video_paths)
 
     if args.delete:
         delete_videos(*args.video_paths)
 
     print(f"영상이 성공적으로 합쳐졌습니다. 결과 파일: {args.output_video}")
 
-# python VideoEditor.py ./movie/final.mp4 --delete ./videos/0001.mp4 ./videos/0002.mp4 ./videos/0003.mp4 ./videos/0004.mp4
+# python VideoConcater.py ./movie/final.mp4 --delete ./videos/0001.mp4 ./videos/0002.mp4 ./videos/0003.mp4 ./videos/0004.mp4
---
+++ src/main/resources/util/VideoConverter.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import time
+import ffmpeg
+import cv2
+from moviepy.editor import VideoFileClip, AudioFileClip
+
+def convert_webm_to_webm(video_path, output_file):
+    cap = cv2.VideoCapture(video_path)
+
+    if not cap.isOpened():
+        print("Error: Unable to open video file")
+        return
+    fourcc = cv2.VideoWriter_fourcc(*'VP80') 
+    fps = 28
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    out = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
+
+    prev_time = 0
+
+    while True:
+        ret, frame = cap.read()
+        current_time = time.time() - prev_time
+        if not ret:
+            break
+        if current_time > 1./fps:
+            prev_time = time.time()
+            out.write(frame)
+
+    cap.release()
+    out.release()
+
+
+def convert_to_mp4(video_path,output_file):
+    try:
+        ffmpeg.input(video_path).output(output_file).run()
+        print(f"File converted successfully: {output_file}")
+    except ffmpeg.Error as e:
+        print(f"Error occurred: {e.stderr}")
+        return
+
+def merge_audio_and_video(video_path, audio_path, output_file):
+    video_clip = VideoFileClip(video_path)
+    audio_clip = AudioFileClip(audio_path,fps=30000)
+
+    video_clip = video_clip.set_audio(audio_clip)
+
+    video_clip.write_videofile(output_file, codec='libx264', audio_codec='aac')
+
+    video_clip.close()
+    audio_clip.close()
+
+def convert_audio(audio_path,output_file):
+    ffmpeg.input(audio_path).output(output_file, codec='libmp3lame').run()
+
+def delete_videos(file_path):
+    try:
+        os.remove(file_path)
+    except Exception as e:
+        print("Error occurred while deleting original files:", e)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python script.py <video_file> <audio_file> [--delete]")
+        sys.exit(1)
+
+    video_path = sys.argv[1]
+    audio_path = sys.argv[2]
+    
+    filename, file_extension = os.path.splitext(video_path)
+    converted_webm_file=f"{filename}_converted.webm"
+    converted_mp4_file=f"{filename}_converted.mp4"
+    converted_mp3_file=f"{filename}_converted.mp3"
+    output_file = f"{filename}.mp4"
+
+    convert_webm_to_webm(video_path,converted_webm_file)
+    convert_to_mp4(converted_webm_file,converted_mp4_file)
+    convert_audio(audio_path,converted_mp3_file)
+    merge_audio_and_video(converted_mp4_file, converted_mp3_file, output_file)
+    
+    if "--delete" in sys.argv:
+        delete_videos(converted_webm_file)
+        delete_videos(converted_mp4_file)
+        delete_videos(converted_mp3_file)
+        # delete_videos(video_path)
+        # delete_videos(audio_path)
+
+# python VideoConverter.py ./movie/0004.webm ./movie/0004.mp3 --delete
